397f05b65e2836fe3e87a1991a3b9ac755642899
marco76tv <marco.sottana@gmail.com>
2023-10-18 18:53:09 +0200

    .

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ const mix = require('laravel-mix');
 require('laravel-mix-jigsaw');
 
 mix.disableSuccessNotifications();
+
 mix.setPublicPath('source/assets/build');
 
 mix.js('source/_assets/js/main.js', 'js')
