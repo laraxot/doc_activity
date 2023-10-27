55feb6d3b61ef4c0bfa29677debb0e14b4bdfe22
marco76tv <marco.sottana@gmail.com>
2023-10-27 17:25:03 +0200

    rebase

--- navigation.php
+++ navigation.php
@@ -10,7 +10,11 @@ return [
             'Navigation' => 'docs/navigation',
             'Algolia DocSearch' => 'docs/algolia-docsearch',
             'Custom 404 Page' => 'docs/custom-404-page',
+<<<<<<< HEAD
             'Test Page' => 'doc_activity/docs/test',
+=======
+            'Test Page' => 'doc_activity/docs/page-test',
+>>>>>>> b5f82e01 (.)
         ],
     ],
     'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
