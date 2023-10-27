e325b39a9881da8a79d77f50539acef808aaef74
marco76tv <marco.sottana@gmail.com>
2023-10-27 23:36:18 +0200

    rebase

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -11,7 +11,11 @@
     <meta property="og:title" content="{{ $page->title ? $page->title . ' | ' : '' }}{{ $page->siteName }}" />
     <meta property="og:description" content="{{ $page->description ?? $page->siteDescription }}" />
     <meta property="og:url" content="{{ $page->getUrl() }}" />
+<<<<<<< HEAD
     <meta property="og:image" content="{{ url('/assets/img/logo.png') }}" />
+=======
+    <meta property="og:image" content="/assets/img/logo.png" />
+>>>>>>> 03337a71 (.)
     <meta property="og:type" content="website" />
 
     <meta name="twitter:image:alt" content="{{ $page->siteName }}">
@@ -34,6 +38,7 @@
 
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i"
         rel="stylesheet">
+<<<<<<< HEAD
     <link rel="stylesheet" href="{{ url('assets/build/css/main.css') }}">
 
 
@@ -51,6 +56,22 @@
                     <img class="h-8 md:h-10 mr-3" src="{{ url('/assets/img/logo.svg') }}"
                         alt="{{ $page->siteName }} logo" />
 
+=======
+    <link rel="stylesheet" href="{{ mix('css/main.css', 'doc_activity/assets/build') }}">
+
+    @if ($page->docsearchApiKey && $page->docsearchIndexName)
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/docsearch.js@2/dist/cdn/docsearch.min.css" />
+    @endif
+</head>
+
+<body class="flex flex-col justify-between min-h-screen bg-gray-100 text-gray-800 leading-normal font-sans">
+    <header class="flex items-center shadow bg-white border-b h-24 mb-8 py-4" role="banner">
+        <div class="container flex items-center max-w-8xl mx-auto px-4 lg:px-8">
+            <div class="flex items-center">
+                <a href="/" title="{{ $page->siteName }} home" class="inline-flex items-center">
+                    <img class="h-8 md:h-10 mr-3" src="/assets/img/logo.svg" alt="{{ $page->siteName }} logo" />
+
+>>>>>>> 03337a71 (.)
                     <h1 class="text-lg md:text-2xl text-blue-900 font-semibold hover:text-blue-600 my-0 pr-4">
                         {{ $page->siteName }}</h1>
                 </a>
@@ -70,7 +91,11 @@
         @yield('body')
     </main>
 
+<<<<<<< HEAD
     <script src="{{ url('assets/build/js/main.js') }}"></script>
+=======
+    <script src="{{ mix('js/main.js', 'doc_activity/assets/build') }}"></script>
+>>>>>>> 03337a71 (.)
 
     @stack('scripts')
 
