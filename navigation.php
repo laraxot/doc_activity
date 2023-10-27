434875aa3ee915ee92b24a29927b7ad5360fabb2
marco76tv <marco.sottana@gmail.com>
2023-10-27 23:36:56 +0200

    rebase

--- navigation.php
+++ navigation.php
@@ -12,13 +12,19 @@ return [
             'Custom 404 Page' => 'docs/custom-404-page',
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 55feb6d3 (rebase)
             'Test Page' => 'doc_activity/docs/test',
 =======
             'Test Page' => 'doc_activity/docs/page-test',
 >>>>>>> b5f82e01 (.)
+<<<<<<< HEAD
 =======
             'Test Page' => 'doc_activity/docs/test',
 >>>>>>> 187dd0fc (.)
+=======
+>>>>>>> 55feb6d3 (rebase)
         ],
     ],
     'Jigsaw Docs' => 'https://jigsaw.tighten.co/docs/installation',
