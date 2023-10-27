9b0899320cb1e644af8ff3064b39e024a616f785
marco76tv <marco.sottana@gmail.com>
2023-10-27 23:36:37 +0200

    rebase

--- source/index.blade.php
+++ source/index.blade.php
@@ -11,10 +11,15 @@
                 <p class="text-lg">Give your documentation a boost with Jigsaw. <br class="hidden sm:block">Generate elegant,
                     static docs quickly and easily.</p>
 
+<<<<<<< HEAD
                 <div class="flex my-10">
                     <a href="{{ $page->url('/docs/getting-started') }}" title="{{ $page->siteName }} getting started"
                         class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get
                         Started!</a>
+=======
+            <div class="flex my-10">
+                <a href="{{ $page->getUrl('/docs/getting-started') }}" title="{{ $page->siteName }} getting started" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
+>>>>>>> 8e34e91d (.)
 
                     <a href="https://jigsaw.tighten.co" title="Jigsaw by Tighten"
                         class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">About
